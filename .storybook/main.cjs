0dc9e124b9524e100c32d2a815284240c4c9b87e
Breno Oliveira <brenolincon8@gmail.com>
2022-10-19 12:38:17 -0300

    Projeto finalizado

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -3,7 +3,8 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y'
   ],
   framework: '@storybook/react',
   core: {
@@ -20,3 +21,5 @@ module.exports = {
     return config
   }
 }
+
+// Aula parou 27 min
